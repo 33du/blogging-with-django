afd6f796fa95a7aebb9fc3c865823ade7ec3f657
33 <du.shanshan@outlook.com>
2018-06-13 22:51:44 +0200

    change css

--- static/js/detail.js
+++ static/js/detail.js
@@ -1,18 +1,5 @@
 $(document).ready(function() {
   $(".carousel-indicators > li:first").addClass("active");
   $(".carousel-item:first").addClass("active");
-
-  var badges = ["badge-success",
-                "badge-info",
-                "badge-warning",
-                "badge-danger",
-                "badge-secondary",
-                "badge-primary"
-                ];
-
-  for (var i = 0; i < $(".info-container span").length; i++) {
-    var badge = badges[i % badges.length];
-    $('.badge[id=' + i + ']').addClass(badge);
-  }
-
+  $(".post-text > p > img").parent().css("text-align", "center");
 });
